be3f5066ac7eaa061b25197e06d85def2af0832f
atharvbajare <109531543+atharvbajare@users.noreply.github.com>
2025-10-29 15:34:08 +0530

    Change S3 backend region to ap-south-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "atharv-terraform-bucket-369"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamo-table"
   }
 }
